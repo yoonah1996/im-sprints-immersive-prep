2caf847e2f2e339937e605c0b521d6520123fcf1
LEE HYEONSEOK <simon@LEEui-MacBookPro.local>
2020-02-04 13:10:45 +0900

    changed by hyun

--- test-example/__test__/math.test.js
+++ test-example/__test__/math.test.js
@@ -2,9 +2,21 @@ const { add, substract, multiply, divide } = require("../math");
 
 // 1. Create test case to check add function works what it is supposed to be.
 
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
 // 2. Create test case to check substract function works what it is supposed to be.
 
+test('substracts 5 - 3 to equal 2', () => {
+    expect(substract(5, 3)).toBe(2);
+  });
+
 // 3. Create test case to check multiply function works what it is supposed to be.
+
+test('multiplys 3 * 7 to equal 21', () => {
+  expect(multiply(3, 7)).toBe(21);
+});
 
 // 4. Create test case to check divide function works what it is supposed to be.
 
