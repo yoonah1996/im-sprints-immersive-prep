450bc420e2eba47a0b574b8404b45e13bb6b9c23
LEE HYEONSEOK <simon@LEEui-MacBookPro.local>
2020-02-04 14:23:09 +0900

    adapt ESlint

--- test-example/math.js
+++ test-example/math.js
@@ -1,25 +1,17 @@
 // Refactor functions below, if you need to
-const add = (num1, num2) => {
-  return num1 + num2;
-};
+const add = (num1, num2) => num1 + num2;
 
-const substract = (num1, num2) => {
-  return num1 - num2;
-};
+const substract = (num1, num2) => num1 - num2;
 
-const multiply = (num1, num2) => {
-  return num1 * num2;
-};
+const multiply = (num1, num2) => num1 * num2;
 
-const divide = (num1, num2) => {
-  return num1 / num2;
-};
+const divide = (num1, num2) => num1 / num2;
 
 module.exports = {
   add,
   substract,
   multiply,
-  divide
+  divide,
 };
 
-console.log("disable eslint on this line")
+console.log('disable eslint on this line');
