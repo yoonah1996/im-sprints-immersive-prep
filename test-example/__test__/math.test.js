17cb281988af00b3bc1bb0004cc0eaa99fae9532
river <river9028@gmail.com>
2020-02-04 11:44:33 +0900

    1st commit by river9028

--- test-example/__test__/math.test.js
+++ test-example/__test__/math.test.js
@@ -2,7 +2,15 @@ const { add, substract, multiply, divide } = require("../math");
 
 // 1. Create test case to check add function works what it is supposed to be.
 
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
 // 2. Create test case to check substract function works what it is supposed to be.
+
+test('substracts 5 - 3 to equal 2', () => {
+    expect(substract(5, 3)).toBe(2);
+  });
 
 // 3. Create test case to check multiply function works what it is supposed to be.
 
